d90d30dbafdd88982db9bd71b018c82e71d4bf9f
Fer Vittorello <fvittorello@gmail.com>
2020-09-29 20:12:44 -0300

    feat(App.js) add custom theme component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
+import theme from 'utility/theme';
 import './App.css';
 
-import Login from 'views/Login';
+// import Login from 'views/Login';
 import RegisterView from 'views/Register';
 
 function App() {
 	// return <div>Hello World React</div>;
-	return <RegisterView />;
+	// console.log(theme);
+	return (
+		<ThemeProvider theme={theme}>
+			<RegisterView />
+		</ThemeProvider>
+	);
 }
 
 export default App;
