d8d42be5c6cc4b04f4ae3f77248fd50dacdfdb77
Fer Vittorello <fvittorello@gmail.com>
2020-09-30 18:40:54 -0300

    feat(ErrorBoundary.js) add error catch and clean unused styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { ErrorBoundary } from 'components/ErrorBoundary';
 import { GlobalStyle } from 'style_utils/globalStyle';
 import theme from 'style_utils/theme';
-import './App.css';
 
 // import Login from 'views/Login';
 import RegisterView from 'views/Register';
@@ -10,11 +10,13 @@ import RegisterView from 'views/Register';
 function App() {
 	// console.log(theme);
 	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<GlobalStyle />
-			<RegisterView />
-		</ThemeProvider>
+		<ErrorBoundary>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<GlobalStyle />
+				<RegisterView />
+			</ThemeProvider>
+		</ErrorBoundary>
 	);
 }
 
--- src/style_utils/globalStyle.js
+++ src/style_utils/globalStyle.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  
+	body {
+		margin: 0;
+	}
+
+	code {
+		font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+	}
 `;
-// Inside the template strings will be declared the font of the project later
