4caf30cd3d28b9475f848b31d80d2ecbf41c5989
Fer Vittorello <fvittorello@gmail.com>
2020-10-07 00:07:25 -0300

    feat(common/endpoints) add API endpoints as a barrel export

---
+++ src/common/endpoints/index.jsx
@@ -0,0 +1,21 @@
+export const endpoints = {
+	login: 'login',
+
+	users_get_all: 'usuarios',
+	users_get_id: `usuarios/`,
+	users_post: 'usuarios',
+	users_put: `usuarios/`,
+	users_del: `usuarios/`,
+
+	orders_get_all: 'pedidos',
+	orders_get_id: `pedidos/`,
+	orders_post: 'pedidos',
+	orders_put: `pedidos/`,
+	orders_del: `pedidos/`,
+
+	products_get_all: 'productos',
+	products_get_id: `productos/`,
+	products_post: 'productos',
+	products_put: `productos/`,
+	products_del: `productos/`,
+};
